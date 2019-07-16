ec825cb3543de22cb8946f3280256fe7e81aa8d4
Dominika Piechota <piechota.domi@gmail.com>
2019-07-16 22:13:17 +0200

    initial commit'

---
+++ src/main/java/com/dodomanika/rest/webservices/hourly/hourlyrestwebservice/HourlyRestWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.dodomanika.rest.webservices.hourly.hourlyrestwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HourlyRestWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HourlyRestWebserviceApplication.class, args);
+	}
+
+}
