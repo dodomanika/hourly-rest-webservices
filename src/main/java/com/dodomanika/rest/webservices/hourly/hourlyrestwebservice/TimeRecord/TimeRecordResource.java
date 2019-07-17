3f3a8269a5600415eae1f0cce63ca8e1bf7abec5
Dominika Piechota <piechota.domi@gmail.com>
2019-07-17 13:24:10 +0200

    time record rest controller added

---
+++ src/main/java/com/dodomanika/rest/webservices/hourly/hourlyrestwebservice/TimeRecord/TimeRecordResource.java
@@ -0,0 +1,21 @@
+package com.dodomanika.rest.webservices.hourly.hourlyrestwebservice.TimeRecord;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class TimeRecordResource {
+
+    @Autowired
+    private TimeResourceHardcodedService timeRecordService;
+
+    @GetMapping("/users/{username}/dates/{date}")
+    public List<TimeRecord> getAllTimeRecordsByDate(@PathVariable String username,
+                                                    @PathVariable int date){
+        return timeRecordService.findAllByDate(date);
+    }
+}
