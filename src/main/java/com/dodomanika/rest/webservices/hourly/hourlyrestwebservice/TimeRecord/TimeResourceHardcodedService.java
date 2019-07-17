226f7dea55769cf23a4a5fde8c8e08b78c9d99b4
Dominika Piechota <piechota.domi@gmail.com>
2019-07-17 13:22:52 +0200

    hardcoded time record service added

---
+++ src/main/java/com/dodomanika/rest/webservices/hourly/hourlyrestwebservice/TimeRecord/TimeResourceHardcodedService.java
@@ -0,0 +1,38 @@
+package com.dodomanika.rest.webservices.hourly.hourlyrestwebservice.TimeRecord;
+
+import org.springframework.stereotype.Service;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class TimeResourceHardcodedService {
+
+    private static List<TimeRecord> timeRecords = new ArrayList<>();
+    private static int idCounter = 0;
+
+    String pattern = "yyyy-MM-dd";
+    SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+
+    static {
+        timeRecords.add(new TimeRecord(++idCounter, "domi", new Date(), "Research on technologies", new Date(), new Date()));
+        timeRecords.add(new TimeRecord(++idCounter, "domi", new Date(), "Evaluate the Top Up functionality", new Date(), new Date()));
+        timeRecords.add(new TimeRecord(++idCounter, "domi", new Date(), "Write the Top Up functionality", new Date(), new Date()));
+    }
+
+    public List<TimeRecord> findAllByDate(int dateFromToday){
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DAY_OF_MONTH, dateFromToday);
+        Date dateFromRequest = calendar.getTime();
+
+        String formattedDate = simpleDateFormat.format(dateFromRequest);
+        return timeRecords.stream()
+                .filter(timeRecord -> formattedDate.equals(simpleDateFormat.format(timeRecord.getDate())))
+                .collect(Collectors.toList());
+    }
+}
