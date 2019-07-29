c507ed61bff123c17a64e39c6ce605d12956b2f7
Dominika Piechota <piechota.domi@gmail.com>
2019-07-29 19:24:20 +0200

    adding a BasicAuthenticationController

--- src/main/java/com/dodomanika/rest/webservices/hourly/hourlyrestwebservice/basic/auth/BasicAuthenticationController.java
+++ src/main/java/com/dodomanika/rest/webservices/hourly/hourlyrestwebservice/basic/auth/BasicAuthenticationController.java
@@ -1,4 +1,15 @@
 package com.dodomanika.rest.webservices.hourly.hourlyrestwebservice.basic.auth;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
 public class BasicAuthenticationController {
+
+    @GetMapping(path = "/basicauth")
+    public AuthenticationBean authenticate(){
+        return new AuthenticationBean("You are authenticated");
+    }
 }
