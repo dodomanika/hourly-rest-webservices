647d0707e26b5da3d1cb8a6096ef33703bff48a2
Dominika Piechota <piechota.domi@gmail.com>
2019-08-04 13:44:24 +0200

    adding an SQL file with the initial data in database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into time_record(id, username, date, task, start_time, duration)
+values(10001, 'in28minutes', sysdate(), 'Write a report', '10:12', 7);
+
+insert into time_record(id, username, date, task, start_time, duration)
+values(10002, 'in28minutes', sysdate(), 'Learn JPA', '10:12', 2);
+
+insert into time_record(id, username, date, task, start_time, duration)
+values(10003, 'in28minutes', sysdate(), 'Estimate functionality', '10:12', 3);
