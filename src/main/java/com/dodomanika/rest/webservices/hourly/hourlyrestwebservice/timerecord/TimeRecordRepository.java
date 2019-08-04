a0982861480f33f945da7644e6fa31655f59d591
Dominika Piechota <piechota.domi@gmail.com>
2019-08-04 14:42:47 +0200

    adding a findAllByDate method to the time record repository

--- src/main/java/com/dodomanika/rest/webservices/hourly/hourlyrestwebservice/timerecord/TimeRecordRepository.java
+++ src/main/java/com/dodomanika/rest/webservices/hourly/hourlyrestwebservice/timerecord/TimeRecordRepository.java
@@ -1,6 +1,12 @@
 package com.dodomanika.rest.webservices.hourly.hourlyrestwebservice.timerecord;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
+@Repository
 public interface TimeRecordRepository extends JpaRepository<TimeRecord, Long> {
+    List<TimeRecord> findAllByDate(LocalDate date);
 }
