f937d3656dad1ee9355a42f2e2f659ad7e0893e5
Dominika Piechota <piechota.domi@gmail.com>
2019-08-04 23:14:54 +0200

    using findByUsername method added

--- src/main/java/com/dodomanika/rest/webservices/hourly/hourlyrestwebservice/UserRepository.java
+++ src/main/java/com/dodomanika/rest/webservices/hourly/hourlyrestwebservice/UserRepository.java
@@ -1,7 +1,10 @@
 package com.dodomanika.rest.webservices.hourly.hourlyrestwebservice;
 
-import com.dodomanika.rest.webservices.hourly.hourlyrestwebservice.entity.User;
+import com.dodomanika.rest.webservices.hourly.hourlyrestwebservice.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
 }
--- src/main/java/com/dodomanika/rest/webservices/hourly/hourlyrestwebservice/jwt/JwtInMemoryUserDetailsService.java
+++ src/main/java/com/dodomanika/rest/webservices/hourly/hourlyrestwebservice/jwt/JwtInMemoryUserDetailsService.java
@@ -1,5 +1,8 @@
 package com.dodomanika.rest.webservices.hourly.hourlyrestwebservice.jwt;
 
+import com.dodomanika.rest.webservices.hourly.hourlyrestwebservice.UserRepository;
+import com.dodomanika.rest.webservices.hourly.hourlyrestwebservice.user.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -12,23 +15,28 @@ import java.util.Optional;
 @Service
 public class JwtInMemoryUserDetailsService implements UserDetailsService {
 
-    static List<JwtUserDetails> inMemoryUserList = new ArrayList<>();
+    /*static List<JwtUserDetails> inMemoryUserList = new ArrayList<>();
 
     static {
         inMemoryUserList.add(new JwtUserDetails(1L, "in28minutes",
                 "$2a$10$3zHzb.Npv1hfZbLEU5qsdOju/tk2je6W6PnNnY.c1ujWPcZh4PL6e", "ROLE_USER_2"));
-    }
+    }*/
+
+    @Autowired
+    UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Optional<JwtUserDetails> findFirst = inMemoryUserList.stream()
-                .filter(user -> user.getUsername().equals(username)).findFirst();
 
-        if (!findFirst.isPresent()) {
-            throw new UsernameNotFoundException(String.format("USER_NOT_FOUND '%s'.", username));
-        }
+        Optional<User> foundUser = userRepository.findByUsername(username);
 
-        return findFirst.get();
+        User user = foundUser
+                .orElseThrow(
+                        () -> new UsernameNotFoundException(String.format("USER_NOT_FOUND '%s'.", username)));
+
+        JwtUserDetails userDetails = new JwtUserDetails(user);
+
+        return userDetails;
     }
 
 }
