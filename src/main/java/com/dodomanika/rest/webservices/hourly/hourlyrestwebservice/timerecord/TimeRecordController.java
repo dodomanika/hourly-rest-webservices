f2f815d32893d2b7bdded8c742415042bbbc9ebf
Dominika Piechota <piechota.domi@gmail.com>
2019-08-04 11:49:47 +0200

    renaming the TimeRecordController

--- src/main/java/com/dodomanika/rest/webservices/hourly/hourlyrestwebservice/timerecord/TimeRecordController.java
+++ src/main/java/com/dodomanika/rest/webservices/hourly/hourlyrestwebservice/timerecord/TimeRecordController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
-public class TimeRecordResource {
+public class TimeRecordController {
 
     @Autowired
     private TimeResourceHardcodedService timeRecordService;
