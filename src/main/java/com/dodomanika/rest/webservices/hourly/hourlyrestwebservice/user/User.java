a3cff7857d5dbac1bc6550952cd96c3e95f19a6c
Dominika Piechota <piechota.domi@gmail.com>
2019-08-06 21:16:37 +0200

    refactoring

--- src/main/java/com/dodomanika/rest/webservices/hourly/hourlyrestwebservice/user/User.java
+++ src/main/java/com/dodomanika/rest/webservices/hourly/hourlyrestwebservice/user/User.java
@@ -1,9 +1,8 @@
 package com.dodomanika.rest.webservices.hourly.hourlyrestwebservice.user;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
+import java.util.Objects;
 
 @Entity
 public class User {
@@ -29,25 +28,45 @@ public class User {
         return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getUsername() {
         return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String getPassword() {
         return password;
     }
 
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     public String getRole() {
         return role;
     }
 
+    public void setRole(String role) {
+        this.role = role;
+    }
+
     @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", username='" + username + '\'' +
-                ", password='" + password + '\'' +
-                ", role='" + role + '\'' +
-                '}';
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id);
     }
 }
