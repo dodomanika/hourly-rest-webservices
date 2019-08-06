fdaf171feacc58361744bcac36f9c0988be1060e
Dominika Piechota <piechota.domi@gmail.com>
2019-08-06 21:17:43 +0200

    refactoring

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,6 @@
+insert into user(id, username, password, role)
+values(100001, 'in28minutes', '$2a$10$AOEuSGnS/YExixnfYSGg3uz8pS8clSQr4C1/kxFrLIe7iZMPZ2UyK', 'ROLE_USER_2');
+
 insert into time_record(id, username, date, task, start_time, duration)
 values(10001, 'in28minutes', sysdate(), 'Write a report', '10:12', 7);
 
@@ -6,6 +9,3 @@ values(10002, 'in28minutes', sysdate(), 'Learn JPA', '10:12', 2);
 
 insert into time_record(id, username, date, task, start_time, duration)
 values(10003, 'in28minutes', sysdate(), 'Estimate functionality', '10:12', 3);
-
-insert into user(id, username, password, role)
-values(100001, 'in28minutes', '$2a$10$AOEuSGnS/YExixnfYSGg3uz8pS8clSQr4C1/kxFrLIe7iZMPZ2UyK', 'ROLE_USER_2');
