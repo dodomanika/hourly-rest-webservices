93300f9cf3db3eb714f4acfbd55c442266984f1d
Dominika Piechota <piechota.domi@gmail.com>
2019-08-06 22:42:04 +0200

    inserting data into task table

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,3 +9,6 @@ values(10002, 'in28minutes', sysdate(), 'Learn JPA', '10:12', 2);
 
 insert into time_record(id, username, date, task, start_time, duration)
 values(10003, 'in28minutes', sysdate(), 'Estimate functionality', '10:12', 3);
+
+insert into task(id, name, description)
+values(20001, 'Write a report', 'The report concerning the bug fixing statistics during the current month');
