082e74aded6b3e8b554a017ceb47901abb0ed911
Dominika Piechota <piechota.domi@gmail.com>
2019-08-06 22:31:57 +0200

    getting all tasks

--- src/main/java/com/dodomanika/rest/webservices/hourly/hourlyrestwebservice/task/TaskController.java
+++ src/main/java/com/dodomanika/rest/webservices/hourly/hourlyrestwebservice/task/TaskController.java
@@ -1,7 +1,21 @@
 package com.dodomanika.rest.webservices.hourly.hourlyrestwebservice.task;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class TaskController {
+
+    @Autowired
+    private TaskRepository repository;
+
+    @GetMapping(path = "/tasks")
+    public List<Task> getTasks(){
+        return repository.findAll();
+    }
+
+
 }
