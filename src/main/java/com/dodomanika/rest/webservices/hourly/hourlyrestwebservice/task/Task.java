1445ef4ea71ee8766670aad1cbefd430324ddd4c
Dominika Piechota <piechota.domi@gmail.com>
2019-08-06 22:02:28 +0200

    adding task entity

---
+++ src/main/java/com/dodomanika/rest/webservices/hourly/hourlyrestwebservice/task/Task.java
@@ -0,0 +1,62 @@
+package com.dodomanika.rest.webservices.hourly.hourlyrestwebservice.task;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+public class Task {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+    private String description;
+
+    public Task() {
+    }
+
+    public Task(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Task task = (Task) o;
+        return Objects.equals(id, task.id);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id);
+    }
+}
