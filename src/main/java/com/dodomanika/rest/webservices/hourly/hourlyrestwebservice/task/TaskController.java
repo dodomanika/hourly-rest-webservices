792f35e636163caa5e32fe35a3cbfb36495f112e
Dominika Piechota <piechota.domi@gmail.com>
2019-08-06 22:02:54 +0200

    adding task controller

---
+++ src/main/java/com/dodomanika/rest/webservices/hourly/hourlyrestwebservice/task/TaskController.java
@@ -0,0 +1,7 @@
+package com.dodomanika.rest.webservices.hourly.hourlyrestwebservice.task;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TaskController {
+}
