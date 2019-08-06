b026fd89f1f90db97770330613144f2e1b2c69a8
Dominika Piechota <piechota.domi@gmail.com>
2019-08-06 22:42:43 +0200

    getting a task by id, deleting a task

--- src/main/java/com/dodomanika/rest/webservices/hourly/hourlyrestwebservice/task/TaskController.java
+++ src/main/java/com/dodomanika/rest/webservices/hourly/hourlyrestwebservice/task/TaskController.java
@@ -1,11 +1,12 @@
 package com.dodomanika.rest.webservices.hourly.hourlyrestwebservice.task;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class TaskController {
 
@@ -15,6 +16,16 @@ public class TaskController {
     @GetMapping(path = "/tasks")
     public List<Task> getTasks(){
         return repository.findAll();
+    }
+
+    @GetMapping(path = "/tasks/{id}")
+    public Optional<Task> getTask(@PathVariable Long id){
+        return repository.findById(id);
+    }
+
+    @DeleteMapping(path = "/tasks/{id}")
+    public void deleteTask(@PathVariable Long id){
+        repository.deleteById(id);
     }
 
 
